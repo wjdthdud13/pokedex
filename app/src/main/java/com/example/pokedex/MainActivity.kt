79fc7ab0c2400549ee1ee2b9a43eafb56c26ce03
wjdthdud13 <sy07222@gmail.com>
2020-07-19 01:00:45 +0900

    pokedex first commit

---
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pokedex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.lang.String.format
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val pokemon = findViewById<RecyclerView>(R.id.pokemon_recycler_view)
+
+        val dataset = arrayOfNulls<String>(100)
+
+        pokemon.setHasFixedSize(true)
+
+        pokemon.layoutManager = GridLayoutManager(this,3)
+
+        var i = 0
+        while (i < 100) {
+            dataset[i] = format(Locale.ENGLISH, "Data_0%d", i)
+            i++
+        }
+        pokemon.adapter = MainAdapter(dataset)
+    }
+}
