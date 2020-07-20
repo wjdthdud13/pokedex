35aad6f07a1f57358e957fbdb0d9a9eea3c5671b
wjdthdud13 <sy07222@gmail.com>
2020-07-20 21:21:14 +0900

    image view変更

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -12,19 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val width = applicationContext.resources.displayMetrics.widthPixels / 3
+
         val pokemon = findViewById<RecyclerView>(R.id.pokemon_recycler_view)
 
         val dataset = arrayOfNulls<String>(100)
 
         pokemon.setHasFixedSize(true)
 
-        pokemon.layoutManager = GridLayoutManager(this,3)
+        pokemon.layoutManager = GridLayoutManager(this, 3)
 
         var i = 0
         while (i < 100) {
             dataset[i] = format(Locale.ENGLISH, "Data_0%d", i)
             i++
         }
-        pokemon.adapter = MainAdapter(dataset)
+        pokemon.adapter = MainAdapter(dataset, width)
+
     }
 }
--- app/src/main/java/com/example/pokedex/MainAdapter.kt
+++ app/src/main/java/com/example/pokedex/MainAdapter.kt
@@ -4,39 +4,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class MainAdapter(private val myDataset: Array<String?>) :
+
+class MainAdapter(private val myDataset: Array<String?>, private val width: Int) :
     RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
 
     class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MyViewHolder {
-        //        // create a new view
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.main_item_view, parent, false)
-        // set the view's size, margins, paddings and layout parameters
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.main_item_view, parent, false)
 
         return MyViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         val textView = holder.view.findViewById<TextView>(R.id.item_view)
-        textView.text =  myDataset[position]
+        textView.text = myDataset[position]
 
         val imageView = holder.view.findViewById<ImageView>(R.id.image_view)
-        Glide.with(holder.view.context).load("https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png").into(imageView)
+        imageView.layoutParams = RelativeLayout.LayoutParams(this.width, this.width)
+
+        Glide.with(holder.view.context)
+            .load("https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png").into(imageView)
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = myDataset.size
 }
